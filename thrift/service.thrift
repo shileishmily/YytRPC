31b7a7f17b248de6e546884bc119998b3d914b02
shilei <shilei@youguu.com>
2016-09-22 02:28:45 +0800

    init

---
+++ thrift/service.thrift
@@ -0,0 +1,21 @@
+service FriendChatThriftRpcService {
+
+}
+
+
+service InfoThriftRpcService {
+
+}
+
+
+service ProductThriftRpcService {
+
+}
+
+service AdThriftRpcService {
+
+}
+
+service UserThriftRpcService {
+
+}
